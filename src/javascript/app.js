6f7e6b30379c240ff6fa19ccf87e1535c36c3b14
SHIN_NARITA <triplfjump1417@gmail.com>
2017-11-02 11:06:03 +0900

    2017-11-02 mapの追加、wrapper用mixinの追加、jQueryの修正

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,17 +1,14 @@
 $(function() {
-  var $productList = $('.js-products_list');
-  var $top = $('.js-top');
-  var $topLogo = $('.js-top__logo');
-  var $topNav = $('.js-top__nav');
-  var $purpose = $('.js-purpose');
-  var $category = $('.js-category');
-  var $icon = $('.p-products__icon');
-  var $scroll = $('.js-scroll').height();
-  var $movieBtn = $('.js-movie__btn');
+  var $productList = $('.js-products_list'),
+      $top = $('.js-top'),
+      $topLogo = $('.js-top__logo'),
+      $topNav = $('.js-top__nav'),
+      $purpose = $('.js-purpose'),
+      $icon = $('.p-products__icon'),
+      $scroll = $('.js-scroll').height();
 
-  $(window).on('reload resize', function() {
+  $(window).on('resize', function() {
     $scroll = $('.js-scroll').height();
-    console.log($scroll);
   });
 
   $(window).on('scroll', function() {
@@ -28,6 +25,36 @@ $(function() {
       $purpose.removeClass('is-position__purpose');
     }
 });
+
+  $('.js-category').on({
+    'mouseenter': function() {
+      $icon.removeClass()
+      .addClass('p-products__icon u-fr js-category_icon js-icon-' + $(this).val());
+    },
+    'click': function() {
+      var value = $(this).val();
+
+      $productList.find('li').each(function() {
+        var categoryId = $(this).val();
+
+        if (value === categoryId) {
+          $(this).show();
+        } else if(value === 1) {
+          $(this).show();
+        } else {
+          $(this).hide();
+        }
+      });
+    }
+  });
+
+  $('.js-movie__btn').on('click', function() {
+    $(this).hide();
+    console.log($('.js-movie'));
+    $('.js-movie')[0].play();
+  });
+
+
 
   $.ajax({
     url: '../../api/open_sohko_design.json',
@@ -52,7 +79,7 @@ $(function() {
           '<a href="">'+
             '<figure class="p-products__img u-fs-13">'+
               '<div class="p-products__img-trim">'+
-                '<img src="'+ products.image +'" alt="products">'+
+                '<img src="'+ products.image +'" alt="products" class="p-products__img__item">'+
                 '</div>'+
               '<figcaption class="p-products__cap">'+
                 '<div class="p-products__item__icon" style="background-image: url(http://opensohko.com/admin/wp-content/themes/opensohkodesign/images/icon/category/icon_'+ products.category +'.png)"></div>'+
@@ -67,75 +94,4 @@ $(function() {
         $productList.append(items);
     });
   };
-
-  $category.on('mouseenter', function() {
-    switch($(this).val()) {
-      case 1:
-        $icon.css({'background-image': 'url(http://opensohko.com/admin/wp-content/themes/opensohkodesign/images/icon/category/icon_all.png)'});
-      break;
-      case 2:
-        $icon.css({'background-image': 'url(http://opensohko.com/admin/wp-content/themes/opensohkodesign/images/icon/category/icon_door.png)'});
-      break;
-      case 3:
-        $icon.css({'background-image': 'url(http://opensohko.com/admin/wp-content/themes/opensohkodesign/images/icon/category/icon_stair.png)'});
-      break;
-      case 4:
-        $icon.css({'background-image': 'url(http://opensohko.com/admin/wp-content/themes/opensohkodesign/images/icon/category/icon_ramp.png)'});
-      break;
-      case 5:
-        $icon.css({'background-image': 'url(http://opensohko.com/admin/wp-content/themes/opensohkodesign/images/icon/category/icon_shelf.png)'});
-      break;
-      case 6:
-        $icon.css({'background-image': 'url(http://opensohko.com/admin/wp-content/themes/opensohkodesign/images/icon/category/icon_table.png)'});
-      break;
-      case 7:
-        $icon.css({'background-image': 'url(http://opensohko.com/admin/wp-content/themes/opensohkodesign/images/icon/category/icon_chair.png)'});
-      break;
-      case 8:
-        $icon.css({'background-image': 'url(http://opensohko.com/admin/wp-content/themes/opensohkodesign/images/icon/category/icon_plant.png)'});
-      break;
-      case 9:
-        $icon.css({'background-image': 'url(http://opensohko.com/admin/wp-content/themes/opensohkodesign/images/icon/category/icon_facade.png)'});
-      break;
-      case 10:
-        $icon.css({'background-image': 'url(http://opensohko.com/admin/wp-content/themes/opensohkodesign/images/icon/category/icon_roof.png)'});
-      break;
-      case 11:
-        $icon.css({'background-image': 'url(http://opensohko.com/admin/wp-content/themes/opensohkodesign/images/icon/category/icon_wall.png)'});
-      break;
-      case 12:
-        $icon.css({'background-image': 'url(http://opensohko.com/admin/wp-content/themes/opensohkodesign/images/icon/category/icon_floor.png)'});
-      break;
-      case 13:
-        $icon.css({'background-image': 'url(http://opensohko.com/admin/wp-content/themes/opensohkodesign/images/icon/category/icon_window.png)'});
-      break;
-
-      default:
-      break;
-    };
-  });
-
-  $category.on('click', function() {
-    var value = $(this).val();
-
-    $productList.find('li').each(function() {
-      var categoryId = $(this).val();
-
-      if (value === categoryId) {
-        $(this).show();
-      } else if(value === 1) {
-        $(this).show();
-      } else {
-        $(this).hide();
-      }
-    })
-  });
-
-
-
-  $movieBtn.on('click', function() {
-    $movieBtn.hide();
-    console.log($('.js-movie'));
-    $('.js-movie')[0].play();
-  });
 });
