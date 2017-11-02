3fc856b8a773beca702d35d9b355b95cd6f8dd6f
SHIN_NARITA <triplfjump1417@gmail.com>
2017-11-02 11:54:46 +0900

    2017-11-02　カテゴリーをjsonファイルで記述し、ajax通信で取得、描画を実施

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,5 +1,6 @@
 $(function() {
-  var $productList = $('.js-products_list'),
+  var $categoryList = $('.p-products__categories__inner'),
+      $productList = $('.js-products_list'),
       $top = $('.js-top'),
       $topLogo = $('.js-top__logo'),
       $topNav = $('.js-top__nav'),
@@ -7,26 +8,26 @@ $(function() {
       $icon = $('.p-products__icon'),
       $scroll = $('.js-scroll').height();
 
-  $(window).on('resize', function() {
-    $scroll = $('.js-scroll').height();
+  $(window).on({
+    'resize': function() {
+      $scroll = $('.js-scroll').height();
+    },
+    'scroll': function() {
+      if ($(this).scrollTop() > $scroll) {
+        $top.addClass('is-position-top');
+        $topLogo.addClass('is-position-top__logo');
+        $topNav.addClass('is-position-top__nav');
+        $purpose.addClass('is-position__purpose');
+      } else {
+        $top.removeClass('is-position-top');
+        $topLogo.removeClass('is-position-top__logo');
+        $topNav.removeClass('is-position-top__nav');
+        $purpose.removeClass('is-position__purpose');
+      }
+    }
   });
 
-  $(window).on('scroll', function() {
-
-    if ($(this).scrollTop() > $scroll) {
-      $top.addClass('is-position-top');
-      $topLogo.addClass('is-position-top__logo');
-      $topNav.addClass('is-position-top__nav');
-      $purpose.addClass('is-position__purpose');
-    } else {
-      $top.removeClass('is-position-top');
-      $topLogo.removeClass('is-position-top__logo');
-      $topNav.removeClass('is-position-top__nav');
-      $purpose.removeClass('is-position__purpose');
-    }
-});
-
-  $('.js-category').on({
+  $categoryList.on({
     'mouseenter': function() {
       $icon.removeClass()
       .addClass('p-products__icon u-fr js-category_icon js-icon-' + $(this).val());
@@ -46,15 +47,26 @@ $(function() {
         }
       });
     }
-  });
+  }, '.js-category');
 
   $('.js-movie__btn').on('click', function() {
     $(this).hide();
-    console.log($('.js-movie'));
     $('.js-movie')[0].play();
   });
 
-
+  $.ajax({
+    url: '../../api/category.json',
+    dataType: 'json',
+  })
+  .done(function(data) {
+    showCategories(data);
+  })
+  .fail(function() {
+    console.log("error");
+  })
+  .always(function() {
+    console.log("complete");
+  });
 
   $.ajax({
     url: '../../api/open_sohko_design.json',
@@ -69,6 +81,19 @@ $(function() {
   .always(function() {
     console.log("complete");
   });
+
+  function showCategories(data) {
+    $categoryList.children().remove();
+
+    data.categories.forEach(function(categories) {
+      var items =
+            '<li class="js-category" value="'+ categories.categoryId +'">'+
+              '<a class="p-products__link u-txt-bold">'+ categories.name +'</a>'+
+            '</li>';
+
+      $categoryList.append(items);
+    });
+  }
 
   function showProducts(data) {
     $productList.children().remove();
